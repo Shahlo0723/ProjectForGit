7dc13fcdbae0b1c947a2a2fd7fd62e0f6db7d9ac
Shahlo <liliyazinnurova005@gmail.com>
2025-01-03 20:08:30 +0500

    7 chi o'garish

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Assalomu alekum !!!");
 
-        System.out.println("Valekum assalom !!!");
+        System.out.println("Assalomu alekum !!!");
+
 
     }
 }
